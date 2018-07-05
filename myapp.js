276c6a4253604ff723895cbdac605701ba764bf5
Dominique <batistadominique@hotmail.com>
2018-07-05 11:01:47 +0100

    Debugging + code comments

--- myapp.js
+++ myapp.js
@@ -4,10 +4,10 @@
 
     my_app.controller('documenterController', ['$scope','$location',
         function($scope, $location) {
-            let base_url = 'schemas/';
+            let base_url = 'https://w3id.org/dats/schema/';
             let schema_file = getUrlFromUrl()["url"];
             let fetch_url = "";
-            if (typeof schema_file != 'undefined'){
+            if (typeof schema_file !== 'undefined'){
                 fetch_url = base_url + schema_file + '.json';
             }
             else{
@@ -15,71 +15,75 @@
             }
             var json_schema = this;
             json_schema.loaded_specs = {};
-            loadJSON(fetch_url, 0, 'none');
+            loadJSON(fetch_url, 0, 'none', null);
 
-            console.log(json_schema);
-
-            function loadJSON(json_file, lvl, field){
-                json_file = json_file.replace("#", "");
+            function loadJSON(json_file, lvl, parent_field, parent_type){
                 let spec_name = json_file.replace('schemas/', '').replace(".json", "");
 
-                /* Level is 0: main spec */
-                if (lvl == 0){
+                // When lvl = 0 it means we are processing the most upper spec
+                if (lvl === 0){
                     json_schema.main_spec = parseJson(json_file);
-                    for (let key in json_schema.main_spec.properties){
-                        if (typeof json_schema.main_spec.properties[key]['$ref'] != 'undefined'){
-                            loadJSON(base_url+json_schema.main_spec.properties[key]['$ref'], lvl+1, key);
-                        }
-                        if (typeof json_schema.main_spec.properties[key].items != 'undefined'){
-                            loadJSON(base_url+json_schema.main_spec.properties[key].items['$ref'], lvl+1, key);
-                            if (typeof json_schema.main_spec.properties[key].items['$ref'] != 'undefined'){
-                                loadJSON(base_url+json_schema.main_spec.properties[key].items['$ref'], lvl+1, key);
+                    seek_subSpecs(json_schema.main_spec.properties, json_schema.main_spec['title']);
+                }
+
+                /* level > 0 means it's a sub spec */
+                else{
+                    let referencingParent = parent_type+" : "+parent_field;
+
+                    // If the spec hasn't been loaded yet
+                    if (typeof json_schema.loaded_specs[spec_name] === 'undefined'){
+
+                        // Parse the json schema
+                        json_schema.loaded_specs[spec_name] = parseJson(json_file);
+
+                        // If the result isn't false
+                        if (json_schema.loaded_specs[spec_name]){
+
+                            // Create an empty array to display the object the field is referenced from
+                            if (typeof json_schema.loaded_specs[spec_name]['referencedFrom'] === 'undefined'){
+                                json_schema.loaded_specs[spec_name]['referencedFrom'] = [];
                             }
-                            else{
-                                if (json_schema.main_spec.properties[key].items.anyOf != 'undefined'){
-                                    for (let sub_item in json_schema.main_spec.properties[key].items.anyOf){
-                                        let new_spec = json_schema.main_spec.properties[key].items.anyOf[sub_item]['$ref'];
-                                        loadJSON(base_url+new_spec, lvl+1, key);
-                                    }
-                                }
+
+                            // Add the object name: field to it
+                            if (json_schema.loaded_specs[spec_name]['referencedFrom'].indexOf(referencingParent) === -1){
+                                json_schema.loaded_specs[spec_name]['referencedFrom'].push(referencingParent);
+                            }
+                        }
+                        seek_subSpecs(json_schema.loaded_specs[spec_name]['properties'], json_schema.loaded_specs[spec_name]['title']);
+                    }
+
+                    // If the spec has already been loaded
+                    else{
+                        if (spec_name !== "undefined"){
+                            if (json_schema.loaded_specs[spec_name]['referencedFrom'].indexOf(referencingParent) === -1){
+                                json_schema.loaded_specs[spec_name]['referencedFrom'].push(referencingParent);
                             }
                         }
                     }
                 }
+            }
 
-                /* Subspecs */
-                else{
-
-                    if (typeof json_schema.loaded_specs[spec_name] == 'undefined'){
-                        json_schema.loaded_specs[spec_name] = parseJson(json_file);
-                        if (json_schema.loaded_specs[spec_name]){
-                            if (typeof json_schema.loaded_specs[spec_name]['referencedFrom'] == 'undefined'){
-                                json_schema.loaded_specs[spec_name]['referencedFrom'] = [];
-                            }
-                            if (json_schema.loaded_specs[spec_name]['referencedFrom'].indexOf(field) == -1){
-                                json_schema.loaded_specs[spec_name]['referencedFrom'].push(field);
-                            }
-                        }
-
-                        for (let key in json_schema.loaded_specs[spec_name]){
-
-                            if (typeof json_schema.loaded_specs[spec_name][key]['$ref'] != 'undefined'){
-                                loadJSON(base_url+json_schema.loaded_specs[spec_name][key]['$ref'], lvl+1, key);
-                            }
-                            if (typeof json_schema.loaded_specs[spec_name][key].items != 'undefined'){
-                                if (typeof json_schema.loaded_specs[spec_name][key].items['$ref'] != 'undefined'){
-                                    loadJSON(base_url+json_schema.loaded_specs[spec_name][key].items['$ref'], lvl+1, key);
-                                }
-                                if (typeof json_schema.loaded_specs[spec_name][key].items['anyOf'] != 'undefined'){
-                                    console.log(json_schema.loaded_specs[spec_name][key].items);
-                                }
-                            }
-                        }
+            function seek_subSpecs(properties, parent_name) {
+                // iterate over the loaded spec and try to locate if sub specs need to be loaded
+                for (let property in properties) {
+                    // Structure is root[key]['$ref']
+                    if (typeof properties[property]['$ref'] !== 'undefined'){
+                        loadJSON(base_url+properties[property]['$ref'], 1, property, parent_name);
                     }
-                    else{
-                        if (spec_name != "undefined"){
-                            if (json_schema.loaded_specs[spec_name]['referencedFrom'].indexOf(field) == -1){
-                                json_schema.loaded_specs[spec_name]['referencedFrom'].push(field);
+
+                    // Structure is root[key]['items']
+                    if (typeof properties[property].items !== 'undefined'){
+
+                        // Structure is root[key]['items']['$ref']
+                        if (typeof properties[property].items['$ref'] !== 'undefined'){
+                            loadJSON(base_url+properties[property].items['$ref'], 1, property, parent_name);
+                        }
+
+                        // Structure is root[key]['items']['anyOf']
+                        if (properties[property].items.anyOf !== 'undefined'){
+                            for (let sub_item in properties[property].items.anyOf){
+                                let new_spec = properties[property].items.anyOf[sub_item]['$ref'];
+                                loadJSON(base_url+new_spec, 1, property, parent_name);
                             }
                         }
                     }
@@ -94,6 +98,7 @@
                     return JSON.parse(request.responseText);
                 }
                 catch(e){
+                    console.warn("Error loading: "+src);
                     return false;
                 }
             }
@@ -161,7 +166,7 @@
         // In the return function, we must pass in a single parameter which will be the data we will work on.
         // We have the ability to support multiple other parameters that can be passed into the filter optionally
         return function(input) {
-            let output = input.replace('#', '').replace('.json', '');
+            let output = input.replace('#', '').replace('.json', '').replace('https://w3id.org/dats/schema/', '');
             return output;
         }
 
